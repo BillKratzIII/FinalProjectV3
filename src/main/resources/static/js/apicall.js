4e2dba9d2dff77c1f7219f9dcea673c76aa57ad3
BillKratzIII <wkratz3@gmail.com>
2016-10-12 13:29:54 -0400

    tested create user function with all styling brought in

---
+++ src/main/resources/static/js/apicall.js
@@ -0,0 +1,11 @@
+$.ajax({
+    url: 'https://jsonplaceholder.typicode.com/', // The URL to the API. You can get this in the API page of the API you intend to consume
+    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+    data: {}, // Additional parameters here
+    dataType: 'json',
+    success: function(data) { console.dir((data.source)); },
+    error: function(err) { alert(err); },
+    beforeSend: function(xhr) {
+    xhr.setRequestHeader("X-Mashape-Authorization", "YOUR-MASHAPE-KEY"); // Enter here your Mashape key
+    }
+});
