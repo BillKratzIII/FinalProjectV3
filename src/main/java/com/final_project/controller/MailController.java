c3739d565ff8a2c1c23ccc4a72d717c84de8f449
Forrest <forrest.corry@gmail.com>
2016-10-12 09:56:08 -0400

    updates

--- src/main/java/com/final_project/controller/MailController.java
+++ src/main/java/com/final_project/controller/MailController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.final_project.config.SmtpMailSender;
-import com.final_project.entity.*;
 
 @RestController
 public class MailController {
