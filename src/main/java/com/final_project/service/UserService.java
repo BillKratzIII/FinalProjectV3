e5a9b56cbca17a4e9baa066a028bc90c372094d2
BillKratzIII <wkratz3@gmail.com>
2016-10-18 09:58:15 -0400

    Tuesday AM

--- src/main/java/com/final_project/service/UserService.java
+++ src/main/java/com/final_project/service/UserService.java
@@ -6,9 +6,11 @@ import com.final_project.entity.User;
 public interface UserService {
 	List<User> getAllUsers();
 	User getUserById(int user_id);
+	User getUserByEmail(String email);
 	boolean addUser(User user);
 	void updateUser(User user);
 	void deleteUser(int user_id);
+	boolean verification(User userFromDb, User userLoggingIn);
 	
 }
 
