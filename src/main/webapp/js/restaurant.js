d1fab95b03644ac00ce9c1ba70f373e73613255d
John <dangelo0011@gmail.com>
2016-10-19 11:44:25 -0400

    lunchtime 10.19.16

--- src/main/webapp/js/restaurant.js
+++ src/main/webapp/js/restaurant.js
@@ -85,26 +85,36 @@ $(function() {
  //    }
     ];
 
-  
+    $.each(restaurants, function(){
+    	$('.restMap').append(
+    	        	
+    	)
+    })
 
 
         $.each(restaurants, function(){
         $('.restMatches').append("<div class=\"col-sm-6 col-md-4\">"
-                    + "<div class=\"thumbnail\">"
-                    + "<img class=\"img-responsive img-rounded\" src=\"" + this.img + "\">"
-                    + "<div class=\"caption\">"
-                    + "<h3>" + this.rest_name + "</h3>"
-                    + "<p>" + this.rest_bio + "</p>"
-                    + "<address>" +this.rest_address + "<br>" + this.rest_city +"," +this.rest_state + " " + this.rest_zip + "<br><abbr title=\"Phone\">P:</abbr>" + this.rest_phone + "</address>" 
-                    + "<p><a href=\"#\" class=\"btn btn-default\" role=\"button\">Directions</a>"
-                    + "</div"
-                    + "</div"
-                    + "</div"
+                + "<div class=\"thumbnail\">"
+                + "<img class=\"img-responsive img-rounded\" src=\"" + this.img + "\">"
+                + "<div class=\"caption\">"
+                + "<h3>" + this.rest_name + "</h3>"
+                + "<p>" + this.rest_bio + "</p>"
+                + "<address>" +this.rest_address + "<br>" + this.rest_city +"," +this.rest_state + " " + this.rest_zip + "<br><abbr title=\"Phone\">P:</abbr>" + this.rest_phone + "</address>" 
+                + "<p><a href=\"#\" class=\"btn btn-default\" role=\"button\">Directions</a>"
+                + "</div>"
+                + "</div>"
+                + "</div>"
          
                       )
 
         $('#restBackground').addClass(this.country);
                 
         });
+        
+        
+        
+                    
+           
+        
     });
 
